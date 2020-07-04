bf8518b5ddd03337aa2656b0dd149a1dfbc33818
whynot0723 <zhanyan@utexas.edu>
2020-07-04 18:56:09 -0500

    change js-question
    uncomment upload img

--- pages/question/question.js
+++ pages/question/question.js
@@ -51,114 +51,114 @@ Page({
     }
   },
 // ////input count/////////////
-// handleTitleInput(e) {
-//   const value = e.detail.value
-//   this.data.title = value
-//   this.data.titleCount = value.length
-//   $digest(this)
-// },
+handleTitleInput(e) {
+  const value = e.detail.value
+  this.data.title = value
+  this.data.titleCount = value.length
+  $digest(this)
+},
 
-// handleContentInput(e) {
-//   const value = e.detail.value
-//   this.data.content = value
-//   this.data.contentCount = value.length
-//   $digest(this)
-// },
+handleContentInput(e) {
+  const value = e.detail.value
+  this.data.content = value
+  this.data.contentCount = value.length
+  $digest(this)
+},
 
-//   ///////////question-input///////////////////////
-//   onLoad(options) {
-//     $init(this)
-//   },
+  ///////////question-input///////////////////////
+  onLoad(options) {
+    $init(this)
+  },
 
-//   handleTitleInput(e) {
-//     const value = e.detail.value
-//     this.data.title = value
-//     this.data.titleCount = value.length
-//     $digest(this)
-//   },
+  handleTitleInput(e) {
+    const value = e.detail.value
+    this.data.title = value
+    this.data.titleCount = value.length
+    $digest(this)
+  },
 
-//   handleContentInput(e) {
-//     const value = e.detail.value
-//     this.data.content = value
-//     this.data.contentCount = value.length
-//     $digest(this)
-//   },
+  handleContentInput(e) {
+    const value = e.detail.value
+    this.data.content = value
+    this.data.contentCount = value.length
+    $digest(this)
+  },
 
-//   chooseImage(e) {
-//     wx.chooseImage({
-//       count: 3,
-//       sizeType: ['original', 'compressed'],
-//       sourceType: ['album', 'camera'],
-//       success: res => {
-//         const images = this.data.images.concat(res.tempFilePaths)
-//         this.data.images = images.length <= 3 ? images : images.slice(0, 3)
-//         $digest(this)
-//       }
-//     })
-//   },
+  chooseImage(e) {
+    wx.chooseImage({
+      count: 3,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: res => {
+        const images = this.data.images.concat(res.tempFilePaths)
+        this.data.images = images.length <= 3 ? images : images.slice(0, 3)
+        $digest(this)
+      }
+    })
+  },
 
-//   removeImage(e) {
-//     const idx = e.target.dataset.idx
-//     this.data.images.splice(idx, 1)
-//     $digest(this)
-//   },
+  removeImage(e) {
+    const idx = e.target.dataset.idx
+    this.data.images.splice(idx, 1)
+    $digest(this)
+  },
 
-//   handleImagePreview(e) {
-//     const idx = e.target.dataset.idx
-//     const images = this.data.images
+  handleImagePreview(e) {
+    const idx = e.target.dataset.idx
+    const images = this.data.images
 
-//     wx.previewImage({
-//       current: images[idx],
-//       urls: images,
-//     })
-//   },
+    wx.previewImage({
+      current: images[idx],
+      urls: images,
+    })
+  },
 
-//   submitForm(e) {
-//     const title = this.data.title
-//     const content = this.data.content
+  submitForm(e) {
+    const title = this.data.title
+    const content = this.data.content
 
-//     if (title && content) {
-//       const arr = []
+    if (title && content) {
+      const arr = []
 
-//       for (let path of this.data.images) {
-//         arr.push(wxUploadFile({
-//           url: config.urls.question + '/image/upload',
-//           filePath: path,
-//           name: 'qimg',
-//         }))
-//       }
+      for (let path of this.data.images) {
+        arr.push(wxUploadFile({
+          url: config.urls.question + '/image/upload',
+          filePath: path,
+          name: 'qimg',
+        }))
+      }
 
-//       wx.showLoading({
-//         title: '正在创建...',
-//         mask: true
-//       })
+      wx.showLoading({
+        title: '正在创建...',
+        mask: true
+      })
 
-//       Promise.all(arr).then(res => {
-//         return res.map(item => JSON.parse(item.data).url)
-//       }).catch(err => {
-//         console.log(">>>> upload images error:", err)
-//       }).then(urls => {
-//         return createQuestion({
-//           title: title,
-//           content: content,
-//           images: urls
-//         })
-//       }).then(res => {
-//         const pages = getCurrentPages();
-//         const currPage = pages[pages.length - 1];
-//         const prevPage = pages[pages.length - 2];
+      Promise.all(arr).then(res => {
+        return res.map(item => JSON.parse(item.data).url)
+      }).catch(err => {
+        console.log(">>>> upload images error:", err)
+      }).then(urls => {
+        return createQuestion({
+          title: title,
+          content: content,
+          images: urls
+        })
+      }).then(res => {
+        const pages = getCurrentPages();
+        const currPage = pages[pages.length - 1];
+        const prevPage = pages[pages.length - 2];
 
-//         prevPage.data.questions.unshift(res)
-//         $digest(prevPage)
+        prevPage.data.questions.unshift(res)
+        $digest(prevPage)
 
-//         wx.navigateBack()
-//       }).catch(err => {
-//         console.log(">>>> create question error:", err)
-//       }).then(() => {
-//         wx.hideLoading()
-//       })
-//     }
-//   }
+        wx.navigateBack()
+      }).catch(err => {
+        console.log(">>>> create question error:", err)
+      }).then(() => {
+        wx.hideLoading()
+      })
+    }
+  }
 
   /**
    * Page initial data
