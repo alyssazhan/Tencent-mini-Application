18cc56cc2de15aff38b9d5e97052d7c3e57cf54a
whynot0723 <zhanyan@utexas.edu>
2020-07-27 14:06:54 -0500

    update evaluation ui

--- pages/questionDetail/questionDetail.js
+++ pages/questionDetail/questionDetail.js
@@ -5,15 +5,38 @@ Page({
    * Page initial data
    */
   data: {
-
+    ansname: "Lucy",//回答者姓名
+    ansuserimg: "../../images/people2.jpeg",//回答者头像
+    anscontent: "首先。。。其次。。。然后",//回答内容
+    anstime: "2020.02.02 17.59.02", //回答时间
+    ansimg:"../../images/testOrderPage.jpg",
+    quesuserimg: "../../images/people1.jpeg",//提问者头像
+    quesname: "Bob",//提问者姓名
+    quescontent: "2020了如何选校。。。",//提问内容
+    questime: "2020.02.02 17.59.02", //提问时间
+    quesimg:"../../images/testOrderPage.jpg",
+    listenPrice:"1",
+    hiddenToast: false,
+    price:"1",
+    people:"1",
+  },
+    /**
+   * 点击相关按钮引发的吐司效果
+   */
+  hiddenToast: function () {
+    var that = this;
+    that.setData({
+      hiddenToast: true
+    })
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-
+   
   },
+  
 
   /**
    * Lifecycle function--Called when page is initially rendered
